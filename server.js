7730492824f6965b5c5afa1656eb456939a8540b
TheMCraft <business@themcraft.com>
2026-01-14 21:18:35 +0100

    changed backend port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 // Upload directory
 const uploadsDir = path.join(__dirname, 'uploads');
